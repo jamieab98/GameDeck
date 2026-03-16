e7739d98ead561ed1948597016caef53d708db62
Jamie Bryson <jamieab98@gmail.com>
2026-03-16 19:02:53 -0400

    Created the outline of logic.py

---
+++ backend/games/tic_tac_toe/logic.py
@@ -0,0 +1,14 @@
+def new_board():
+    board = ['', '', '', '', '', '', '', '', '']
+
+def apply_move(board, player, location):
+    return
+
+def check_winner(board):
+    return
+
+def check_tie(board):
+    return
+
+def next_player(player):
+    return 
