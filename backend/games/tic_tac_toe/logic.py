2dcdf4dd8f6a9f485e8f94ca4f3970af2552d3a0
Jamie Bryson <jamieab98@gmail.com>
2026-03-16 19:11:14 -0400

    Updated the outline for the ttt logic

--- backend/games/tic_tac_toe/logic.py
+++ backend/games/tic_tac_toe/logic.py
@@ -2,13 +2,33 @@ def new_board():
     board = ['', '', '', '', '', '', '', '', '']
 
 def apply_move(board, player, location):
+    #make sure the move is valid
+
+    #apply the player's move to the location
+
+    #update the board
+
     return
 
 def check_winner(board):
+    #see if the winning conditions have been met
+
+    #if a player has won, return the winner
+
+    #if no player has won, do nothing
+    
     return
 
 def check_tie(board):
+    #see if the board has been filled with no winning conditions
+
+    #if a game is a tie, return that the game is a tie
+
     return
 
 def next_player(player):
+    #check who just took a valid turn
+    
+    #switch to the other player
+
     return 
