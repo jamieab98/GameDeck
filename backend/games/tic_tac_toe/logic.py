59e7b51a57686c4fd52dda36da2ef9c6747ed662
Jamie Bryson <jamieab98@gmail.com>
2026-03-16 20:03:22 -0400

    completed check_winner()

--- backend/games/tic_tac_toe/logic.py
+++ backend/games/tic_tac_toe/logic.py
@@ -19,13 +19,20 @@ def apply_move(board, player, location):
     return new_board
 
 def check_winner(board):
+    winner_situations = [(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
     #see if the winning conditions have been met
+    for situation in winner_situations:
+        check = []
+        for position in situation:
+            check.append(board[position])
 
     #if a player has won, return the winner
-
+        if check[0] == check[1] == check[2] and check[0] != '':
+            print("Game over")
+            return {'Winner': check[0]}
+            
     #if no player has won, do nothing
-
-    return
+    return {'Winner': None}
 
 def check_tie(board):
     #see if the board has been filled with no winning conditions
@@ -41,7 +48,6 @@ def next_player(player):
 
     return 
 
-board = ['', '', '', '', '', '', '', '', '']
-players = ["P1", "P2"]
-apply_move(board, "P1", 0)
-apply_move(board, "P2", 1)
+board = ['X', 'X', 'X', '', '', '', '', '', '']
+
+check_winner(board)
