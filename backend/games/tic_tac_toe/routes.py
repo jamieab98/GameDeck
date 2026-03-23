d20ac75f9baa36ade5e6ee5b70c2fa7060fe0a67
Jamie Bryson <jamieab98@gmail.com>
2026-03-23 18:33:29 -0400

    Finished routes.py... for now

--- backend/games/tic_tac_toe/routes.py
+++ backend/games/tic_tac_toe/routes.py
@@ -2,7 +2,7 @@ from logic import *
 from flask import Blueprint, request, jsonify
 from . import tic_tac_toe_bp
 
-@tic_tac_toe_bp.POST('/move')
+@tic_tac_toe_bp.post('/move')
 def move():
     data = request.get_json()
 
@@ -10,4 +10,24 @@ def move():
     player = data['player']
     location = data['location']
 
-    apply_move(board, player, location)
+    new_board = apply_move(board, player, location)
+    
+    if check_winner(new_board):
+        return jsonify({
+            'board': new_board,
+            'player': player,
+            'winner': 'winner'
+        })
+
+    if check_tie(new_board):
+        return jsonify({
+            'board': new_board,
+            'winner': None
+        })
+    
+    upcoming_player = next_player(player)
+    return jsonify({
+        'board': new_board,
+        'player': upcoming_player,
+        'winner': None
+    })
