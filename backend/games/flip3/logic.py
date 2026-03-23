da8dbc1c318e2bc70f4310620e9a483429fcb31c
Jamie Bryson <jamieab98@gmail.com>
2026-03-23 19:59:46 -0400

    created the check_end() function

--- backend/games/flip3/logic.py
+++ backend/games/flip3/logic.py
@@ -10,9 +10,12 @@ def draw_card(current_deck, player_hand):
     pulled_card = deck[random.randint(0 ,len(current_deck)-1)]
     current_deck.remove(pulled_card)
     player_hand.append(pulled_card)
-    return (current_deck, player_hand)
+    print(player_hand)
+    return(current_deck, player_hand)
 
-print(draw_card(current_deck, p1_hand))
-print(draw_card(current_deck, p2_hand))
-print(draw_card(current_deck, p1_hand))
-print(draw_card(current_deck, p2_hand))
+def check_end(player_hand):
+    if len(player_hand) != len(set(player_hand)):
+        print("Player has a duplicate card. This player is out for the round")
+        return(player_hand)
+    else:
+        print("Player may continue")
