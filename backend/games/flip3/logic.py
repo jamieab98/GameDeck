05cece169fbdcb5e6b9c95336a181e8d3ce93b65
Jamie Bryson <jamieab98@gmail.com>
2026-03-23 19:24:11 -0400

    modified draw_card so it adds the removed card to the players hand

--- backend/games/flip3/logic.py
+++ backend/games/flip3/logic.py
@@ -1,11 +1,18 @@
 import random
 
 deck = [0, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5]
-
 current_deck = deck.copy()
 
-def draw_card(current_deck):
+p1_hand = []
+p2_hand = []
+
+def draw_card(current_deck, player_hand):
     pulled_card = deck[random.randint(0 ,len(current_deck)-1)]
     current_deck.remove(pulled_card)
-    return (current_deck, pulled_card)
+    player_hand.append(pulled_card)
+    return (current_deck, player_hand)
 
+print(draw_card(current_deck, p1_hand))
+print(draw_card(current_deck, p2_hand))
+print(draw_card(current_deck, p1_hand))
+print(draw_card(current_deck, p2_hand))
