5c4e41dc2a81c41b86c3e4e6f651df57bc7e6c7c
Jamie Bryson <jamieab98@gmail.com>
2026-03-23 19:15:36 -0400

    created the draw_card function that removes a ranndom card from the deck

--- backend/games/flip3/logic.py
+++ backend/games/flip3/logic.py
@@ -2,6 +2,10 @@ import random
 
 deck = [0, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5]
 
-def draw_card(deck):
-    pulled_card = deck[random.randint(0 ,len(deck)-1)]
-    print(pulled_card)
+current_deck = deck.copy()
+
+def draw_card(current_deck):
+    pulled_card = deck[random.randint(0 ,len(current_deck)-1)]
+    current_deck.remove(pulled_card)
+    return (current_deck, pulled_card)
+
