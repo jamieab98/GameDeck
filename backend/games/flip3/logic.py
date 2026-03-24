2f48b85f59fe728f9956dc3f10b1b743966633d7
Jamie Bryson <jamieab98@gmail.com>
2026-03-23 21:44:04 -0400

    removed the change_turn logic because I'm kinda lost

--- backend/games/flip3/logic.py
+++ backend/games/flip3/logic.py
@@ -3,39 +3,27 @@ import random
 deck = [0, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5]
 current_deck = deck.copy()
 
-p1 = {'player': 'P1', 'hand': [], 'points': 0, 'status': False}
+p1 = {'player': 'P1', 'hand': [], 'points': 0, 'status': True}
 p2 = {'player': 'P2', 'hand': [], 'points': 0, 'status': True}
 
-def draw_card(current_deck, player):
+current_player = p1
+
+def draw_card(current_deck, current_player):
     pulled_card = deck[random.randint(0 ,len(current_deck)-1)]
     current_deck.remove(pulled_card)
-    player['hand'].append(pulled_card)
-    print(player['hand'])
-    return(current_deck, player['hand'])
+    current_player['hand'].append(pulled_card)
+    print(current_player)
+    return(current_deck, current_player)
 
-def check_end(player):
-    if len(player['hand']) != len(set(player['hand'])):
-        player['status'] = False
+def check_end(current_player):
+    if len(current_player['hand']) != len(set(current_player['hand'])):
+        current_player['status'] = False
     else:
-        print("Player may continue")
+        #print("Player may continue")
         return None
 
-def add_points(player):
-    for card in player['hand']:
-        player['points'] += card
-    player['status'] = False
-    return player['points']
-
-def change_turn(player):
-    if p1['status'] == False and p2['status'] == False:
-        next_player = None
-    elif player == p1 and p2['status'] == True:
-        next_player = p2
-    elif player == p1 and p2['status'] == False:
-        next_player = p1
-    elif player == p2 and p1['status'] == True:
-        next_player = p1
-    elif player == p2 and p1['status'] == False:
-        next_player = p2
-    return next_player
-
+def add_points(current_player):
+    for card in current_player['hand']:
+        current_player['points'] += card
+    current_player['status'] = False
+    return current_player['points']
