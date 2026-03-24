17c507e00892b71829597e0f89751990f745c8f1
Jamie Bryson <jamieab98@gmail.com>
2026-03-23 20:19:13 -0400

    changed the structure so p1 hold the hand, points, and the status and each function takes the p1 (or p2) as the parameter

--- backend/games/flip3/logic.py
+++ backend/games/flip3/logic.py
@@ -3,21 +3,30 @@ import random
 deck = [0, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5]
 current_deck = deck.copy()
 
-p1_hand = []
-p2_hand = []
+p1 = {'hand': [], 'points': 0, 'status': True}
+p2 = {'hand': [], 'points': 0, 'status': True}
 
-def draw_card(current_deck, player_hand):
+def draw_card(current_deck, player):
     pulled_card = deck[random.randint(0 ,len(current_deck)-1)]
     current_deck.remove(pulled_card)
-    player_hand.append(pulled_card)
-    print(player_hand)
-    return(current_deck, player_hand)
+    player['hand'].append(pulled_card)
+    print(player['hand'])
+    return(current_deck, player['hand'])
 
-def check_end(player_hand):
-    if len(player_hand) != len(set(player_hand)):
+def check_end(player):
+    if len(player['hand']) != len(set(player['hand'])):
         print("Player has a duplicate card. This player is out for the round")
         return False
     else:
         print("Player may continue")
         return None
 
+def add_points(player):
+    for card in player['hand']:
+        player['points'] += card
+    return player['points']
+
+def change_turn(player):
+    print(player['status'])
+
+change_turn(p1)
