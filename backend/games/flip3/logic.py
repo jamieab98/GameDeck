c32ade1ebe8d2311345e57af570dc78d1bb76c2d
Jamie Bryson <jamieab98@gmail.com>
2026-03-23 20:38:52 -0400

    created the change_turn logic

--- backend/games/flip3/logic.py
+++ backend/games/flip3/logic.py
@@ -3,8 +3,8 @@ import random
 deck = [0, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5]
 current_deck = deck.copy()
 
-p1 = {'hand': [], 'points': 0, 'status': True}
-p2 = {'hand': [], 'points': 0, 'status': True}
+p1 = {'player': 'P1', 'hand': [], 'points': 0, 'status': False}
+p2 = {'player': 'P2', 'hand': [], 'points': 0, 'status': True}
 
 def draw_card(current_deck, player):
     pulled_card = deck[random.randint(0 ,len(current_deck)-1)]
@@ -15,8 +15,7 @@ def draw_card(current_deck, player):
 
 def check_end(player):
     if len(player['hand']) != len(set(player['hand'])):
-        print("Player has a duplicate card. This player is out for the round")
-        return False
+        player['status'] = False
     else:
         print("Player may continue")
         return None
@@ -24,9 +23,19 @@ def check_end(player):
 def add_points(player):
     for card in player['hand']:
         player['points'] += card
+    player['status'] = False
     return player['points']
 
 def change_turn(player):
-    print(player['status'])
+    if p1['status'] == False and p2['status'] == False:
+        next_player = None
+    elif player == p1 and p2['status'] == True:
+        next_player = p2
+    elif player == p1 and p2['status'] == False:
+        next_player = p1
+    elif player == p2 and p1['status'] == True:
+        next_player = p1
+    elif player == p2 and p1['status'] == False:
+        next_player = p2
+    return next_player
 
-change_turn(p1)
